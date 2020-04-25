5f598d47a0829d3c9ff81329a9bcdadf716d1faf
w-tensyo <dr.tensyo@gmail.com>
2020-04-25 13:54:00 +0900

    implement back btn
    # what
    詳細画面など深い階層でヘッダーに戻るボタンを表示できるように実装
    
    # Why
    ユーザービリティ向上のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,5 @@ class UsersController < ApplicationController
   def set_user
 
     @user = User.find(params[:id])
-
-    # if user_signed_in?
-    #   @users = User.find(params[:id])
-    # else
-    #   redirect_to root_path
-    # end
   end
 end
