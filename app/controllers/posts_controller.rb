e1440adc5609496baf1b53d322e17f7e5aa1286d
w-tensyo <dr.tensyo@gmail.com>
2020-04-27 13:12:52 +0900

    refactoring new post system
    # What
    new,createアクションで実装していたpost_newクラスを削除して、attributesとfield_forを利用した実装へ切替
    
    # Why
    投稿内容の拡張をするために必要な修正だったため。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,22 @@ class PostsController < ApplicationController
 before_action :set_post, only: [:show]
 
 
-  class PostForm
-    include ActiveModel::Model #テーブルは持たないが、ApplicationRecordのsaveメソッドなどを提供
+  # class PostForm
+  #   include ActiveModel::Model #テーブルは持たないが、ApplicationRecordのsaveメソッドなどを提供
   
-    attr_accessor :comment, :image, :equipment, :equ_comment, :post_id, :user_id
+  #   attr_accessor :comment, :image, :equipment, :equ_comment, :post_id, :user_id
   
-    validates :image, presence: true
+  #   validates :image, presence: true
   
-    def save
-      return false if invalid? #バリデーションを追加する場合はここに追加
+  #   def save
+  #     return false if invalid? #バリデーションを追加する場合はここに追加
   
-      #postインスタンスを作成。Postsテーブルの各種カラムを引数で指定してあげる。この段階で各種パラメータはnilとなっていると思う
-      post = Post.new(comment: comment,image: image, user_id: user_id)
-      post.gears.new(equipment: equipment,equ_comment: equ_comment, post_id: post_id)
-      post.save
-    end
-  end
+  #     #postインスタンスを作成。Postsテーブルの各種カラムを引数で指定してあげる。この段階で各種パラメータはnilとなっていると思う
+  #     post = Post.new(comment: comment,image: image, user_id: user_id)
+  #     post.gears.new(equipment: equipment,equ_comment: equ_comment, post_id: post_id)
+  #     post.save
+  #   end
+  # end
 
   
   def index
@@ -30,12 +30,20 @@ before_action :set_post, only: [:show]
   end
 
   def new
-    @post_form = PostForm.new
+    # @post_form = PostForm.new
+    @post = Post.new
+    @post.gears.new
   end
 
   def create
-    @post_form = PostForm.new(post_params)
-    if @post_form.save
+    # @post_form = PostForm.new(post_params)
+    # if @post_form.save
+    #   redirect_to root_path, notice: '保存されました'
+    # else
+    #   redirect_to root_path
+    # end
+    @post = Post.new(post_params)
+    if @post.save!
       redirect_to root_path, notice: '保存されました'
     else
       redirect_to root_path
@@ -65,12 +73,15 @@ before_action :set_post, only: [:show]
     @post = Post.find(params[:id])
   end
 
-
   def post_params
-    params.require(:posts_controller_post_form).permit(:comment, :image, :equipment, :equ_comment, :post_id).merge(user_id: current_user.id)
+    params.require(:post).permit(:comment,:image, :user_id, gears_attributes: [:equipment, :equ_comment, :_destroy, :id])
   end
 
+  # def post_params
+  #   params.require(:posts_controller_post_form).permit(:comment, :image, :equipment, :equ_comment, :post_id).merge(user_id: current_user.id)
+  # end
+
   def post_update_params
-    params.require(:post).permit(:comment, :image,gears_attributes: [:equipment,:equ_comment,:_destroy,:id])
+    params.require(:post).permit(:comment, :image, gears_attributes: [:equipment,:equ_comment,:_destroy,:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,12 @@
 class Post < ApplicationRecord
 
   belongs_to :user
+  has_many :comments
   has_many :gears, dependent: :destroy
   accepts_nested_attributes_for :gears,allow_destroy: true
-  has_many :comments
 
-  # # postsテーブルにgearsテーブルをネストさせる処理
-  # accepts_nested_attributes_for :gears
-
+  validates :image, presence: true
+  
   #postsテーブルに対して、画像アップロードを可能にするためマウント
   mount_uploader :image, ImageUploader
 end
